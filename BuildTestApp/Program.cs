f112e6815cd0eb120a92f52697eb8527e522b8c6
Christo du Toit <christo@dutoit.co.uk>
2023-05-12 18:32:57 +0100

    kshfsdkjsfhsdjk

--- BuildTestApp/Program.cs
+++ BuildTestApp/Program.cs
@@ -10,6 +10,8 @@ namespace BuildTestApp
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
