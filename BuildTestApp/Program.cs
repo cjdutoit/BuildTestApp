73676807bcb35b1450a7b6474a4d0023bbd2869f
Christo du Toit <christo@dutoit.co.uk>
2023-05-12 17:25:55 +0100

    Add project files.

---
+++ BuildTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace BuildTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
